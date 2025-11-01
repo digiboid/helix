3a1df44ea2e914eeea072aa77e829fe1c84c53fa
digiboid <64439910+digiboid@users.noreply.github.com>
2025-11-01 12:05:44 +0100

    Disable "written" message

--- helix-term/src/application.rs
+++ helix-term/src/application.rs
@@ -638,10 +638,7 @@ impl Application {
         self.editor
             .set_doc_path(doc_save_event.doc_id, &doc_save_event.path);
         // TODO: fix being overwritten by lsp
-        self.editor.set_status(format!(
-            "'{}' written, {lines}L {size}",
-            get_relative_path(&doc_save_event.path).to_string_lossy(),
-        ));
+        self.editor.set_status(format!(""));
     }
 
     #[inline(always)]
