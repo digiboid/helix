3c197bd42e016e05232a6e3695c3e759372308b1
digiboid <64439910+digiboid@users.noreply.github.com>
2025-11-15 10:23:01 +0100

    Improve display on buffer search
    On the left pane shows line numbers and matched line.

--- helix-term/src/commands.rs
+++ helix-term/src/commands.rs
@@ -2918,11 +2918,15 @@ fn buffer_search(cx: &mut Context) {
     struct BufferResult {
         /// 0 indexed lines
         line_num: usize,
+        line_content: String,
     }
 
     impl BufferResult {
-        fn new(line_num: usize) -> Self {
-            Self { line_num }
+        fn new(line_num: usize, line_content: String) -> Self {
+            Self {
+                line_num,
+                line_content,
+            }
         }
     }
 
@@ -2950,7 +2954,8 @@ fn buffer_search(cx: &mut Context) {
             |item: &BufferResult, config: &BufferSearchConfig| {
                 Cell::from(Spans::from(vec![
                     Span::styled((item.line_num + 1).to_string(), config.number_style),
-                    Span::styled(":", config.colon_style),
+                    Span::styled(": ", config.colon_style),
+                    Span::raw(&item.line_content),
                 ]))
             },
         ),
@@ -2998,9 +3003,12 @@ fn buffer_search(cx: &mut Context) {
 
             let mut searcher = searcher;
             let mut stop = false;
-            let sink = sinks::UTF8(|line_num, _line_content| {
+            let sink = sinks::UTF8(|line_num, line_content| {
                 stop = injector
-                    .push(BufferResult::new(line_num as usize - 1))
+                    .push(BufferResult::new(
+                        line_num as usize - 1,
+                        line_content.trim().to_string(),
+                    ))
                     .is_err();
 
                 Ok(!stop)
@@ -3032,7 +3040,7 @@ fn buffer_search(cx: &mut Context) {
         1, // contents
         [],
         config,
-        move |cx, BufferResult { line_num }, action| {
+        move |cx, BufferResult { line_num, .. }, action| {
             let doc = doc_mut!(cx.editor, &doc_id);
             let view = view_mut!(cx.editor);
             let text = doc.text();
@@ -3051,6 +3059,7 @@ fn buffer_search(cx: &mut Context) {
             }
         },
     )
+    .truncate_start(false)
     .with_preview(move |_editor, BufferResult { line_num, .. }| {
         Some((doc_id.into(), Some((*line_num, *line_num))))
     })
