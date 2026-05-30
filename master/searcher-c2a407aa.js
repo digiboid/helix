0ae647aa6ae757e51cd06b2c93dec9ebbd2cb698
digiboid <digiboid@users.noreply.github.com>
2026-05-30 07:18:44 +0000

    deploy: 00a2933b14e3b75a51002463d7f0a74240a11e99

--- master/searcher-c2a407aa.js
+++ master/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-246b9529.js',
+                path_to_root + 'searchindex-4552ab8d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
